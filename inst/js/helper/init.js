c15395b2ef94e02321c5cb89314117cc8ba63c1d
Shians <registertonysu@gmail.com>
2015-12-22 16:44:47 +1100

    Changed initialisation loop
    - Initialisation now chart type agnostic

--- inst/js/helper/init.js
+++ inst/js/helper/init.js
@@ -1,10 +1,5 @@
+// Cycle through constructed plots
 for (var i=0; i<glimma.chartInfo.length; i++) {
 	var chart = glimma.chartInfo[i];
-
-	// Cycle through constructed plots
-	if (chart.type == "scatter") {
-		d3.select(".glimma-plot.available").datum(glimma.data[i]).call(glimma.charts[i]);
-	} else {
-
-	}
+	d3.select(".glimma-plot.available").datum(glimma.data[i]).call(glimma.charts[i]);
 }
