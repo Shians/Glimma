0ecb875cee1540fd4985a727fe5bb63e17385237
Shians <registertonysu@gmail.com>
2016-04-25 18:52:24 +1000

    Added gltablink function
    Added gltablink function

---
+++ R/gltablink.R
@@ -0,0 +1,28 @@
+#' Plot linkages
+#'
+#' Helper function for writing the link properties in interactive Glimma plots
+#'
+#' @param from the index of the source table.
+#' @param to the index of the plot which receives the event and performs an action.
+#' @param action the action that is performed in the plot.
+#' @param info additional info for creating the link.
+#'
+#' @return a link object containing the plot linking information.
+
+
+gltablink <- function(from, to, action="none", info="none") {
+    out <- list()
+
+    assertClass(from, "numeric")
+    assertClass(to, "numeric")
+    assertClass(action, "character")
+    assertClass(info, "character")
+
+    out <- gllink(from=from, to=to, 
+                    src="click", dest=action, flag="tablink", info=info)
+
+    out$type <- "tablink"
+
+    class(out) <- "jslink"
+    return(out)
+}
