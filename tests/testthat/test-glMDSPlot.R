c4b54e399c877b93f69c883c407021699a84f164
shian_su <registertonysu@gmail.com>
2016-12-15 16:30:03 +1100

    Updated tests

--- tests/testthat/test-glMDSPlot.R
+++ tests/testthat/test-glMDSPlot.R
@@ -1,7 +1,20 @@
 context("Test MDS Plot")
 
-test_that("MDS Plot runs", {
-    load("test_data_MDS.rda")
+test_that("MDS Plot runs for matrix", {
+    load("test_data_MDS_DGEList.rda")
+    counts <- all_counts$counts
+
+    groups <- data.frame(Grouping1=c(rep(1, 40), rep(2, 30)),
+                       Grouping2=c(rep(1, 20), rep(2, 30), rep(3, 20)),
+                       Grouping3=sample(1:4, 70, replace=TRUE))
+
+    expect_silent(glMDSPlot(counts, launch=FALSE))
+    expect_silent(glMDSPlot(counts, groups=groups[, 1], launch=FALSE))
+    expect_silent(glMDSPlot(counts, groups=groups, launch=FALSE))
+})
+
+test_that("MDS Plot runs for DGEList", {
+    load("test_data_MDS_DGEList.rda")
 
     groups <- data.frame(Grouping1=c(rep(1, 40), rep(2, 30)),
                        Grouping2=c(rep(1, 20), rep(2, 30), rep(3, 20)),
@@ -10,6 +23,13 @@ test_that("MDS Plot runs", {
     expect_silent(glMDSPlot(all_counts, launch=FALSE))
     expect_silent(glMDSPlot(all_counts, groups=groups[, 1], launch=FALSE))
     expect_silent(glMDSPlot(all_counts, groups=groups, launch=FALSE))
+})
+
+test_that("MDS Plot runs", {
+    load("test_data_MDS_DESeq.rda")
+
+    expect_silent(glMDSPlot(dds, launch=FALSE))
+
 })
 
 unlink("glimma-plots", recursive=TRUE)
