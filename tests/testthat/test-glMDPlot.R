414a3f842b6f1e227f2815837c2e0c27dabefdfa
Shians <registertonysu@gmail.com>
2017-11-20 12:17:15 +1100

    Refactored tests

--- tests/testthat/test-glMDPlot.R
+++ tests/testthat/test-glMDPlot.R
@@ -62,12 +62,28 @@ test_that("MD Plot runs for DGELRT", {
 
     expect_warning(glMDPlot(qlf, counts=counts, main="MDPlot", launch=FALSE))
 
-    expect_silent(glMDPlot(qlf, anno=geneanno, main="MDPlot", launch=FALSE))
-    expect_silent(glMDPlot(qlf, anno=geneanno, main="MDPlot", xlab="foo", ylab="bar", launch=FALSE))
-    expect_silent(glMDPlot(qlf, anno=geneanno, main="MDPlot", side.xlab="foo", side.ylab="bar", launch=FALSE))
-    expect_silent(glMDPlot(qlf, counts=counts, anno=geneanno, samples=1:6, status=is.de, main="MDPlot", launch=FALSE))
-    expect_silent(glMDPlot(qlf, counts=counts, anno=geneanno, groups=genotypes, samples=1:6, status=is.de, launch=FALSE))
-    expect_silent(glMDPlot(qlf, counts=counts, anno=geneanno, groups=genotypes, samples=1:6, display.columns=display.columns, status=is.de, main="MDPlot", launch=FALSE))
+    # common arguments for working tests
+    glMDPlot_core <- pryr::partial(
+        glMDPlot,
+        x = qlf,
+        anno = geneanno,
+        main = "MDPlot",
+        launch = FALSE
+    )
+
+    expect_silent(glMDPlot_core())
+    expect_silent(glMDPlot_core(xlab="foo", ylab="bar"))
+    expect_silent(glMDPlot_core(side.xlab="foo", side.ylab="bar"))
+
+    # add counts
+    glMDPlot_with_counts <- pryr::partial(
+        glMDPlot_core,
+        count = counts
+    )
+
+    expect_silent(glMDPlot_with_counts(samples=1:6, status=is.de))
+    expect_silent(glMDPlot_with_counts(groups=genotypes, samples=1:6, status=is.de))
+    expect_silent(glMDPlot_with_counts(groups=genotypes, samples=1:6, display.columns=display.columns, status=is.de))
 
     load("little_mait_DGELRT.RData")
 
@@ -80,21 +96,58 @@ test_that("MD Plot runs for DGEExact", {
     counts <- counts$counts
     display.columns <- c("Symbols", "GeneID")
 
-    expect_silent(glMDPlot(et, main="MDPlot", launch=FALSE))
-    expect_silent(glMDPlot(et, main="MDPlot", xlab="foo", ylab="bar", launch=FALSE))
-    expect_silent(glMDPlot(et, main="MDPlot", side.xlab="foo", side.ylab="bar", launch=FALSE))
-    expect_silent(glMDPlot(et, counts=counts, samples=1:6, status=is.de, main="MDPlot", launch=FALSE))
-    expect_silent(glMDPlot(et, counts=counts, anno=geneanno, groups=genotypes, samples=1:6, display.columns=display.columns, status=is.de, main="MDPlot", launch=FALSE))
+    # common arguments for working tests
+    glMDPlot_core <- pryr::partial(
+        glMDPlot,
+        x = et,
+        main = "MDPlot",
+        launch = FALSE
+    )
+
+    expect_silent(glMDPlot_core())
+    expect_silent(glMDPlot_core(xlab="foo", ylab="bar"))
+    expect_silent(glMDPlot_core(side.xlab="foo", side.ylab="bar"))
+
+    # add counts
+    glMDPlot_with_counts_samples_status <- pryr::partial(
+        glMDPlot_core,
+        count = counts,
+        samples = 1:6,
+        status=is.de
+    )
+
+    expect_silent(glMDPlot_with_counts_samples_status())
+    expect_silent(glMDPlot_with_counts_samples_status(anno=geneanno, groups=genotypes, display.columns=display.columns))
 })
 
 test_that("MD Plot runs for DESeqDataSet", {
     library(DESeq2)
     load("test_data_DESeqDataSet.RData")
 
-    expect_silent(glMDPlot(lymphoma_dds, anno=lymphoma_anno, groups=lymphoma_genotypes, launch=FALSE))
-    expect_silent(glMDPlot(lymphoma_dds, anno=lymphoma_anno, groups=lymphoma_genotypes, samples=1:7, status=lymphoma_status, launch=FALSE))
-    expect_silent(glMDPlot(DESeq2::results(lymphoma_dds), DESeq2::counts(lymphoma_dds), anno=lymphoma_anno, groups=lymphoma_genotypes, launch=FALSE))
-    expect_silent(glMDPlot(DESeq2::results(lymphoma_dds), DESeq2::counts(lymphoma_dds), anno=lymphoma_anno, groups=lymphoma_genotypes, samples=1:7, status=lymphoma_status, launch=FALSE))
+    # common arguments for working tests
+    glMDPlot_core <- pryr::partial(
+        glMDPlot,
+        x = lymphoma_dds,
+        anno = lymphoma_anno,
+        groups = lymphoma_genotypes,
+        launch = FALSE
+    )
+
+    expect_silent(glMDPlot_core())
+    expect_silent(glMDPlot_core(samples=1:7, status=lymphoma_status))
+
+    # common arguments for working tests
+    glMDPlot_deseqresults <- pryr::partial(
+        glMDPlot,
+        x = DESeq2::results(lymphoma_dds),
+        counts = DESeq2::counts(lymphoma_dds),
+        anno = lymphoma_anno,
+        groups = lymphoma_genotypes,
+        launch = FALSE
+    )
+
+    expect_silent(glMDPlot_deseqresults())
+    expect_silent(glMDPlot_deseqresults(samples=1:7, status=lymphoma_status))
 
 })
 
