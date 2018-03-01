372928d3d5595c8e0b8a36d0d93f06c0455cfd00
Shians <registertonysu@gmail.com>
2018-03-01 12:52:01 +1100

    Fixed labels with spaces
    Added test for XY plot labels
    
    Added test for XY plot labels with spaces

--- tests/testthat/test-glXYPlot.R
+++ tests/testthat/test-glXYPlot.R
@@ -23,3 +23,17 @@ test_that("XY Plot runs with vector or single column anno", {
 
 	expect_silent(glXYPlot(x=volcano_coef, y=volcano_lod, anno=volcano_anno[, 1, drop=FALSE], launch=FALSE, path=temp_dir))
 })
+
+test_that("XY Plot runs with spaces in labels", {
+    load("volcano_glXYPlot.RData")
+
+    temp_dir <- normalizePath(tempdir())
+
+    expect_silent(
+        glXYPlot(x=volcano_coef, y=volcano_lod, xlab = "coef", ylab = "lod", launch=FALSE, path=temp_dir)
+    )
+
+    expect_silent(
+        glXYPlot(x=volcano_coef, y=volcano_lod, xlab = "volcano coef", ylab = "volcano lod", launch=FALSE, path=temp_dir)
+    )
+})
